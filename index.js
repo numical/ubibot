130b7d26049a907d2eedbcfe7114b006a1564288
Mike Evans <mike.evans@numical.com>
2017-10-31 21:53:43 +0000

    require path spike

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // applies 'use strict' pragma all files loaded after this
 require('use-strict');
 
-const path = require('path');
-
-// to allow root-relative paths
-// explicitly set root so it des not matter if rfr is a peer module
-const rfr = require('rfr');
-rfr.setRoot(path.join(__dirname, 'lib'));
-
 // the public api
-const api = rfr('core/api.js');
+const api = require('core/api.js');
 
 module.exports = api;
